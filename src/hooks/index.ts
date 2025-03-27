d89a81512ea282e3e25b974e00cec3d9760c9515
nghiamv <nghiama0029@gmail.com>
2025-03-27 11:04:06 +0700

    refactor: replace useDebounceMutating with useIsMutating in OBaseForm
    - Removed the useDebounceMutating hook and its implementation.
    - Integrated useIsMutating to manage loading state for form submission.
    - Updated button disable logic to reflect the new loading state management.

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,4 +1,3 @@
 export { default as useFormItems } from './use-form-items';
 export { default as useUrlParams } from './useUrlParams';
 export { default as useDebounce } from './useDebounce';
-export { default as useDebounceMutating } from './useDebounceMutating';
