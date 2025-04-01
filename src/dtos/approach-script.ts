4f7cf9ff0a459f8c0e8c5ff71075a3945d6d86bd
cuongnv <cuongjunocn1@gmail.com>
2025-04-01 16:58:01 +0700

    Revert "feat: add parentId support in attribute management and enhance scenario tree component"
    This reverts commit ea9ed0dff7dce53bef55e64cd5c4079f094c5aa1.

--- src/dtos/approach-script.ts
+++ src/dtos/approach-script.ts
@@ -31,7 +31,6 @@ export interface ApproachScriptAttributeDTO {
   approachResultStep?: ApproachResultStepDTO;
   controlCode?: string;
   controlId?: string;
-  parentId?: string;
 }
 
 export interface ApproachStepFormValue {
